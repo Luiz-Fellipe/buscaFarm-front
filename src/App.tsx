a2ab29d2d6c7991a63f14d95eef27e2ce4fe6d89
lucas <l.dallier@xsensors.ai>
2020-07-04 15:02:30 -0300

    Create Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <h1>Hello</h1>
-  );
-}
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <>
+    <Router>
+      <Routes />
+    </Router>
+  </>
+);
 
 export default App;
