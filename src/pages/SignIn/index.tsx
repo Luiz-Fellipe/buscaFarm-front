7d402459f80df2e57cea0a59e8c63438a788df38
lucas <l.dallier@xsensors.ai>
2020-07-05 23:34:02 -0300

    Implement Page Sign In

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,65 @@
 import React from 'react';
 
-// import { Title } from './styles';
+import LogoBF from '~/assets/images/Logo.png';
+import UserGreen from '~/assets/images/IconUserGreen.png';
+import LockGreen from '~/assets/images/IconLockGreen.png';
+
+import {
+  Container,
+  Content,
+  Logo,
+  User,
+  Password,
+  Forgot,
+  Login,
+  ArrowLeftTop,
+  ArrowRightBottom,
+} from './styles';
 
 const SignIn: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <ArrowLeftTop>
+        <div className="setup">
+          <span className="button">
+            <span className="head" />
+          </span>
+        </div>
+      </ArrowLeftTop>
+      <Content>
+        <Logo>
+          <img src={LogoBF} alt="" />
+        </Logo>
+        <User>
+          <div>
+            <img src={UserGreen} alt="" />
+          </div>
+          <input placeholder="Usuário" />
+        </User>
+        <Password>
+          <div>
+            <img src={LockGreen} alt="" />
+          </div>
+          <input placeholder="Senha" />
+        </Password>
+        <Forgot>
+          <span>esqueceu sua senha?</span>
+        </Forgot>
+        <Login>
+          <button type="button">
+            <span>Entrar</span>
+          </button>
+        </Login>
+      </Content>
+      <ArrowRightBottom>
+        <div className="setup-two">
+          <span className="button-two">
+            <span className="head-two" />
+          </span>
+        </div>
+      </ArrowRightBottom>
+    </Container>
+  );
 };
 
 export default SignIn;
